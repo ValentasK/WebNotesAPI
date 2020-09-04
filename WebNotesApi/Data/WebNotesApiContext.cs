3228440fb41de4149771dcf613fcbbbe09df7c64
Valentas Klimas <valentasklimas@gmail.com>
2020-09-04 15:57:10 +0100

    Add project files.

---
+++ WebNotesApi/Data/WebNotesApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebNotesApi.Models;
+
+namespace WebNotesApi.Data
+{
+    public class WebNotesApiContext : DbContext
+    {
+        public WebNotesApiContext (DbContextOptions<WebNotesApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebNotesApi.Models.Note> Note { get; set; }
+    }
+}
---
+++ WebNotesApi/Migrations/20200903154823_initial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebNotesApi.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Note",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Text = table.Column<string>(nullable: true),
+                    Created = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Note", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Note");
+        }
+    }
+}
